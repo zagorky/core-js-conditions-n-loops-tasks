df1e24fa3d18d3e2dd86c4670bc99ce0d41566e9
zagorky <zagorkiynahyi@gmail.com>
2024-12-15 10:16:49 +0300

    feat: add a solution

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -278,9 +278,25 @@ function isContainNumber(num, digit) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    let leftSum = 0;
+    let rigthSum = 0;
+
+    for (let l = 0; l < i; l += 1) {
+      leftSum += arr[l];
+    }
+    for (let r = i + 1; r < arr.length; r += 1) {
+      rigthSum += arr[r];
+    }
+    if (leftSum === rigthSum) {
+      return i;
+    }
+  }
+  return -1;
 }
+
+// console.log(getBalanceIndex([1, 2, 3, 4, 5]));
 
 /**
  * Generates a spiral matrix of a given size, filled with numbers in ascending order starting from one.
